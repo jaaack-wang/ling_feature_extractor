4a3c0685a3a5200925270e3086e1ddd7a22d277e
jaaack-wang <74220936+jaaack-wang@users.noreply.github.com>
2020-11-25 08:12:54 -0600

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,31 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="linguistic feature extraction",
+    version="0.0.1",
+    packages=['LingFeatureExtractor'],
+    description="A corpus-linguistic tool to extract 95 general linguistic features",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+
+    author="Jack Wang",
+    author_email="jackwang196531@gmail.com",
+
+    url="https://github.com/jaaack-wang/ling_feature_extractor",
+    keywords=['Computational linguistics', 'Corpus-linguistic tool', 'NLP', 'Natural Language Processing'],
+
+    classifiers=[
+        "Intended Audience :: Linguistics",
+        "Intended Audience :: Science/Research",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        'Topic :: Text Processing',
+        'Topic :: Text Processing :: Linguistic',
+    ],
+    python_requires='>=3.6',
+    license="MIT License",
+)
