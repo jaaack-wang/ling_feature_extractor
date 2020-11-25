701c7957893cea7435a128d16f71175bcb3171b5
jaaack-wang <74220936+jaaack-wang@users.noreply.github.com>
2020-11-25 06:40:58 -0600

    Update extractor.py

--- LingFeatureExtractor/extractor.py
+++ LingFeatureExtractor/extractor.py
@@ -1,7 +1,8 @@
-import features_set as fs
-import text_processor as tp
+from LingFeatureExtractor import features_set as fs
+from LingFeatureExtractor import text_processor as tp
 import pandas as pd
 import os
+from time import time
 
 
 def show_feature_names():
@@ -104,19 +105,15 @@ def get_single_file_feature_fre(file_path, normalized_rate=100, save_tagged_file
     if save_tagged_file:
         save_single_tagged_text(file_path)
 
-    num_utterances = fs.num_utterances(raw_text)
-    num_overlaps = fs.num_overlaps(raw_text)
     num_words = fs.word_count(tagged_text)
     num_chars = fs.total_char(tagged_text)
     num_types = len(set(tp.get_texts_lemmas(raw_text)))
 
     # start calculating feature frequency
-    words_per_utter = num_words / num_utterances
-    utter = num_utterances / num_words * 100
+
     mean_word_len = num_chars / num_words
     type_token_ratio = num_types / num_words
-    overlaps = num_overlaps / num_words * 100
-    sub_data.extend([words_per_utter, utter, mean_word_len, type_token_ratio, overlaps])
+    sub_data.extend([mean_word_len, type_token_ratio])
 
     other_feature_patterns = [v for v in fs.FEATURE_DICT.values()]
     for pattern in other_feature_patterns:
@@ -156,7 +153,7 @@ class CorpusLFE:
 
     def corpus_feature_fre_extraction(self, normalized_rate=100, save_tagged_corpus=True,
                                       save_extracted_features=True, left=0, right=0):
-        freq_data = [['Filename', 'Words per utterance', 'Utterance', 'Mean word length', 'Type-token ratio', 'Overlap']]
+        freq_data = [['Filename', 'Mean word length', 'Type-token ratio']]
         feature_names = [k for k in fs.FEATURE_DICT.keys()]
         freq_data[0].extend(feature_names)
         filenames = tp.get_filenames_from_dir(self._file_dir)
@@ -195,3 +192,4 @@ class CorpusLFE:
         filenames = tp.get_filenames_from_dir(self._file_dir)
         for filename in filenames:
             save_extracted_feature_by_regex(self._file_dir + filename, regex, left, right, feature_name)
+
