00172f6b8819cd2a13575f10098e9babd7b6d043
jaaack-wang <74220936+jaaack-wang@users.noreply.github.com>
2020-11-25 07:03:43 -0600

    Update extractor.py

--- LingFeatureExtractor/extractor.py
+++ LingFeatureExtractor/extractor.py
@@ -1,5 +1,5 @@
-from LingFeatureExtractor import features_set as fs
-from LingFeatureExtractor import text_processor as tp
+import features_set as fs
+import text_processor as tp
 import pandas as pd
 import os
 
